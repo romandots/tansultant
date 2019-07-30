abf49985f19ba53c8c3df545e1dadc5c4f09068c
Roman Dots <romandots@brainex.co>
2019-07-30 13:32:10 +0300

    Move DTO\Payment Create createInternalTransaction() and create() method in PaymentRepository

--- app/Repository/DTO/Payment.php
+++ app/Repository/DTO/Payment.php
@@ -8,7 +8,7 @@
 
 declare(strict_types=1);
 
-namespace App\Services\Payment\DTO;
+namespace App\Repository\DTO;
 
 /**
  * Class Payment
--- app/Repository/PaymentRepository.php
+++ app/Repository/PaymentRepository.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Models\Account;
 use App\Models\Payment;
 
 /**
@@ -18,8 +19,61 @@ use App\Models\Payment;
  */
 class PaymentRepository
 {
-    public function create(\App\Services\Payment\DTO\Payment $dto): Payment
+    /**
+     * @param \App\Repository\DTO\Payment $dto
+     * @return Payment
+     * @throws \Exception
+     */
+    public function create(DTO\Payment $dto): Payment
     {
-        return new Payment;
+        $payment = new Payment;
+        $payment->id = \uuid();
+        $payment->status = $dto->status;
+        $payment->type = $dto->type;
+        $payment->transfer_type = $dto->transfer_type;
+        $payment->object_type = $dto->object_type;
+        $payment->object_id = $dto->object_id;
+        $payment->user_id = $dto->user_id;
+        $payment->account_id = $dto->account_id;
+        $payment->external_id = $dto->external_id;
+        $payment->confirmed_at = $dto->confirmed_at;
+        $payment->amount = $dto->amount;
+        $payment->name = $dto->name;
+
+        return $payment;
+    }
+
+    /**
+     * @param DTO\Payment $dto
+     * @param Account $fromAccount
+     * @param Account $toAccount
+     * @return array [$fromAccountPayment, $toAccountPayment]
+     */
+    public function createInternalTransaction(DTO\Payment $dto, Account $fromAccount, Account $toAccount): array
+    {
+        $dto->transfer_type = Payment::TRANSFER_TYPE_INTERNAL;
+        $dto->status = Payment::STATUS_CONFIRMED;
+
+        $firstDto = clone $dto;
+        $secondDto = clone $dto;
+
+        $firstDto->account_id = $fromAccount->id;
+        $firstDto->amount = 0 - $dto->amount;
+
+        $secondDto->account_id = $toAccount->id;
+        $secondDto->amount = $dto->amount;
+
+        return \DB::transaction(function () use ($secondDto, $firstDto) {
+            $firstPayment = $this->create($firstDto);
+            $secondPayment = $this->create($secondDto);
+
+            $firstPayment->related_id = $secondPayment->id;
+            $secondPayment->related_id = $firstPayment->id;
+
+            $firstPayment->save();
+            $secondPayment->save();
+
+            return [$firstPayment, $secondPayment];
+        });
     }
 }
