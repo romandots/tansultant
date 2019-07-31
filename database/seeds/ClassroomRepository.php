bc91436a2b5374d614d4eee03c90dd563f5933c6
Roman Dots <romandots@brainex.co>
2019-07-31 09:38:28 +0300

    Implement CRUD

--- database/seeds/ClassroomRepository.php
+++ database/seeds/ClassroomRepository.php
@@ -10,8 +10,9 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use App\Http\Requests\Api\DTO\Classroom as ClassroomDto;
+use App\Http\Requests\PublicApi\DTO\Classroom as ClassroomDto;
 use App\Models\Classroom;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Class ClassroomRepository
@@ -21,6 +22,7 @@ class ClassroomRepository
     /**
      * @param int $id
      * @return Classroom
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
      */
     public function find(int $id): Classroom
     {
@@ -37,13 +39,51 @@ class ClassroomRepository
     public function create(ClassroomDto $dto): Classroom
     {
         $classroom = new Classroom;
+        $this->fill($dto, $classroom);
+        $classroom->save();
+
+        return $classroom;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getAll(): Collection
+    {
+        return Classroom::query()
+            ->whereNull('deleted_at')
+            ->get();
+    }
+
+    /**
+     * @param Classroom $classroom
+     * @param ClassroomDto $dto
+     */
+    public function update(Classroom $classroom, ClassroomDto $dto): void
+    {
+        $this->fill($dto, $classroom);
+        $classroom->save();
+    }
+
+    /**
+     * @param ClassroomDto $dto
+     * @param Classroom $classroom
+     */
+    private function fill(ClassroomDto $dto, Classroom $classroom): void
+    {
         $classroom->name = $dto->name;
         $classroom->branch_id = $dto->branch_id;
         $classroom->color = $dto->color;
         $classroom->capacity = $dto->capacity;
         $classroom->number = $dto->number;
-        $classroom->save();
+    }
 
-        return $classroom;
+    /**
+     * @param Classroom $classroom
+     * @throws \Exception
+     */
+    public function delete(Classroom $classroom): void
+    {
+        $classroom->delete();
     }
 }
