9478ae851fdf6b80717c8347069fcc3f3496ce4c
Roman Dots <romandots@brainex.co>
2019-12-04 21:27:55 +0300

    Move POST /login to manager_api

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'middleware' => ['api'],
-    'prefix' => 'api/v1'
+    'prefix' => 'manager_api/v1'
 ], static function () {
     Route::post('login', 'AuthController@login');
 });
