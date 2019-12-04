070c4a42fde24e216002654dea66d771ec306185
Roman Dots <romandots@brainex.co>
2019-12-04 05:09:03 +0300

    Fix logout and /user

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,10 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'AuthController@login');
-Route::post('logout', 'AuthController@logout');
 
-Route::get('user', 'UserController@me');
-Route::patch('user/password', 'UserController@updatePassword');
+Route::middleware('auth:api')->group(static function () {
+    Route::post('logout', 'AuthController@logout');
+
+    Route::get('user', 'UserController@me');
+    Route::patch('user/password', 'UserController@updatePassword');
+});
