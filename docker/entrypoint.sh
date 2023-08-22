831930f2edba5e9fbdde14eec8032cfdeee8cce8
romandots <ram.d.kreiz@gmail.com>
2023-08-22 11:24:37 +0300

    Update entrypoint.sh: add chmod update for logs directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ touch /var/log/nginx_access.log
 touch /var/log/nginx_error.log
 chmod 777 /var/log/nginx_access.log
 chmod 777 /var/log/nginx_error.log
+chmod -R 0777 /app/storage/logs
 
 #composer install
 
