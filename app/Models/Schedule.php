70c05fe6dbbf4bedd3f859ebb928eb5f033e59b5
romandots <ram.d.kreiz@gmail.com>
2024-02-23 09:13:31 +0300

    Fix

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -15,6 +15,7 @@ use App\Models\Enum\Weekday;
 use App\Models\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Schedule
@@ -63,6 +64,7 @@ class Schedule extends Model
 {
     use UsesUuid;
     use HasFactory;
+    use SoftDeletes;
 
     public const TABLE = 'schedules';
 
