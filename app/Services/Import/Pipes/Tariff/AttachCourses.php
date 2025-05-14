ea9a5b4d23caf6dda0f8d39ec9d4f04bef974fb7
romandots <ram.d.kreiz@gmail.com>
2025-05-14 15:49:59 +0300

    Import tool: * Fix Tariff import: attaching courses

--- app/Services/Import/Pipes/Tariff/AttachCourses.php
+++ app/Services/Import/Pipes/Tariff/AttachCourses.php
@@ -2,7 +2,6 @@
 
 namespace App\Services\Import\Pipes\Tariff;
 
-use App\Common\DTO\IdsDto;
 use App\Components\Loader;
 use App\Services\Import\Exceptions\ImportException;
 use App\Services\Import\Exceptions\ImportSkippedException;
@@ -14,29 +13,57 @@ class AttachCourses implements \App\Services\Import\Contracts\PipeInterface
 
     public function handle(ImportContext $ctx, Closure $next): ImportContext
     {
-        try {
-            $classes = $ctx->old->default_classes_included
-                ? array_filter(unserialize($ctx->old->default_classes_included, ['allowed_classes' => false]))
-                : [];
-        } catch (\Throwable $e) {
-            $classes = [];
+        $classesIncluded = $ctx->old->default_classes_included;
+
+        if (empty($classesIncluded) || $classesIncluded === 'all') {
+            $classes = $ctx->manager->getOldDatabaseConnection()
+                ->table('classes')
+                ->whereRaw(config('import.map.course.where'))
+                ->get('id')
+                ->pluck('id')
+                ->toArray();
+            $ctx->debug('Получили все классы из старой базы: ' . implode(', ', $classes));
+        } else {
+            try {
+                $classes = array_filter(unserialize($ctx->old->default_classes_included, ['allowed_classes' => false]));
+                $ctx->debug('Получили классы из старой базы: ' . implode(', ', $classes));
+            } catch (\Throwable $e) {
+                $classes = [];
+                $ctx->debug('Ошибка при получении классов из старой базы: ' . $e->getMessage());
+            }
         }
+
+        $classesExcluded = $ctx->old->default_classes_excluded;
+        try {
+            $classesExcluded = array_filter(unserialize($classesExcluded, ['allowed_classes' => false]));
+            $ctx->debug('Получили исключённые классы из старой базы: ' . implode(', ', $classesExcluded));
+        } catch (\Throwable $e) {
+            $classesExcluded = [];
+            $ctx->debug('Ошибка при получении исключённых классов из старой базы: ' . $e->getMessage());
+        }
+
+        $classes = array_diff($classes, $classesExcluded);
+
         if (empty($classes)) {
+            $ctx->debug('Нет классов для привязки к тарифу');
             return $next($ctx);
         }
 
-        $coursesIds = new IdsDto($ctx->adminUser);
-        $coursesIds->id = $ctx->newId;
+        $coursesIds = [];
         foreach ($classes as $classId) {
             try {
-                $coursesIds->relations_ids[] = $ctx->manager->ensureImported('course', $classId, $ctx->level);
-            } catch (ImportSkippedException|ImportException) {
+                $coursesIds[] = $ctx->manager->ensureImported('course', $classId, $ctx->level);
+            } catch (ImportSkippedException|ImportException $exception) {
+                $ctx->debug('Пропускаем импорт класса #' . $classId . ': ' . $exception->getMessage());
                 continue;
             }
         }
 
+        $courses = Loader::courses()->getMany($coursesIds);
+
         try {
-            Loader::tariffs()->findAndAttachCourses($coursesIds);
+            $ctx->debug('Привязываем курсы к тарифу (' . $courses->count() . ' шт.): ' . $courses->implode('name', ', '));
+            Loader::tariffs()->getService()->attachCourses($ctx->newRecord, $courses, $ctx->adminUser);
         } catch (\Throwable $throwable) {
             throw new \App\Services\Import\Exceptions\ImportException(
                 "Ошибка привязки курсов к тарифу ({$ctx->old?->ticket_type_name}): {$throwable->getMessage()}",
