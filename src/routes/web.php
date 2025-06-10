be46804533688a33d36a7798cc8880e1a90030b3
romandots <ram.d.kreiz@gmail.com>
2025-06-10 14:51:56 +0300

    Refactor Docker setup and update environment configurations

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('swagger.yaml', 'Swagger@yaml')->name('swagger.yaml');
-    Route::get('swagger.json', 'Swagger@json')->name('swagger.source');
-    //Route::get('swagger', 'Swagger@ui');
+//    Route::get('swagger.yaml', 'Swagger@yaml')->name('swagger.yaml');
+//    Route::get('swagger.json', 'Swagger@json')->name('swagger.source');
+//    Route::get('swagger', 'Swagger@ui');
 
 
     Route::get('dashboard', 'SystemMonitor@dashboard')->name('monitor.dashboard');
