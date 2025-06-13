8af82e50688f6cd59b9614a16039a3afa3120dae
romandots <ram.d.kreiz@gmail.com>
2025-06-13 12:39:35 +0300

    Refactor test setup to use RefreshDatabase and add HTTP faking

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,13 +2,21 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Http;
 use Tests\Traits\CreatesFakes;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use DatabaseTransactions;
+    use RefreshDatabase;
     use CreatesFakes;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Http::fake();
+    }
 }
